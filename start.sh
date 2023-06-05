abb8715d07b875a11b0a1ea240d3f7cc4e403791
KarunaTG <109456795+KarunaTG@users.noreply.github.com>
2023-06-05 13:17:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/KarunaTG/NataliyaH.git /NataliyaH
+  git clone https://github.com/KarunaTG/Nataliya.git /Nataliya
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /NataliyaH
+  git clone $UPSTREAM_REPO /Nataliya
 fi
-cd /NataliyaH
+cd /Nataliya
 pip3 install -U -r requirements.txt
 echo "Starting Nataliya...."
 python3 bot.py
