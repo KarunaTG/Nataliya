e97d34ee82cf996383029f02f4e4bef34ec8c7ec
KarunaTG <109456795+KarunaTG@users.noreply.github.com>
2023-09-28 08:47:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,7 +47,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-IS_VERIFY =  is_enabled (( environ . get ( 'IS_VERIFY' , ' False ' ) ), False )
+IS_VERIFY =  is_enabled (( environ . get ( 'IS_VERIFY' , ' True ' ) ), True )
 HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', "https://t.me/Verify_Toturial")
 VERIFY2_URL = environ.get('VERIFY2_URL', "easysky.in")
 VERIFY2_API = environ.get('VERIFY2_API', "c0c9fb160a5d33bb141ce117e2cce939a36a9682")
