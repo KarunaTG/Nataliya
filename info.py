55382e159c274e33e5763403e4630812718c24ca
KarunaTG <109456795+KarunaTG@users.noreply.github.com>
2024-04-19 15:35:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,10 +43,10 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://natashamajee:PpWbAa03XOygSpu7@cluster.q8yyke6.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://natashamajee:UTtgMu3qoV3PMxxQ@cluster.q8yyke6.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
-SECONDDB_URI = environ.get('SECONDDB_URI', None)
+SECONDDB_URI = environ.get('SECONDDB_URI', "mongodb+srv://natashamajee:UTtgMu3qoV3PMxxQ@cluster.q8yyke6.mongodb.net/?retryWrites=true&w=majority")
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
