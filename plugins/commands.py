a22a514eb728195f05e4daf82a3fd6c4992f6477
KarunaTG <109456795+KarunaTG@users.noreply.github.com>
2024-04-19 15:57:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -53,12 +53,6 @@ async def start(client, message):
         m=await message.reply_text("👀")
         await asyncio.sleep(1)
         await m.delete()
-        m=await message.reply_text("⚡")
-        await asyncio.sleep(1)
-        await m.delete()
-        m=await message.reply_sticker("CAACAgUAAxkBAAEKryFlRmcR5uIta374YeU0qWGpl9pU2gACMgsAAgdt8FR9xcT0MTT_EzME") 
-        await asyncio.sleep(1)
-        await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
