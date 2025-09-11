4ad550d639d1c4bcb83d9a9de3a0342e640cc614
KarunaTG <109456795+KarunaTG@users.noreply.github.com>
2025-09-11 23:27:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,10 +50,10 @@ SECONDDB_URI = environ.get('SECONDDB_URI', "mongodb+srv://shieldbot:Karuna100@cl
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shortxlinks.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'df61a3ee34cb5514124b67c91146cc7b49fcccbf')
-SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', 'shortxlinks.com')
-SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', 'df61a3ee34cb5514124b67c91146cc7b49fcccbf')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'softurl.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', '33b9762e55077b291dd3f0e3792079640f7f112f')
+SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', 'softurl.in')
+SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', '33b9762e55077b291dd3f0e3792079640f7f112f')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '-1002081411946').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "10")
